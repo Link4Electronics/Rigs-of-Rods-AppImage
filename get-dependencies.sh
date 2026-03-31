8d3348c8bebb46ef0a5faf2aa7c36c94bc180f95
Link4Electronics <link4electronics@gmail.com>
2026-03-30 22:42:36 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -20,7 +20,7 @@ echo "Getting app..."
 echo "---------------------------------------------------------------"
 itchio-downloader --url "https://rigs-of-rods.itch.io/rigs-of-rods" --platform linux --downloadDirectory .
 mkdir -p ./AppDir/bin
-tar -xvf game-2066563.zip
+bsdtar -xvf game-2066563.zip
 rm -f *.zip
 mv -v RoR plugins.cfg resources languages content ./AppDir/bin
 mv -v lib/* /usr/lib
