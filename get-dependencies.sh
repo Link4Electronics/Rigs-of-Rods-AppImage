b49d64169fbe78fc562a72a98659a4216b1fecde
Link4Electronics <link4electronics@gmail.com>
2026-03-30 22:39:35 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm openal
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -18,8 +18,8 @@ make-aur-package itchio-downloader
 # If the application needs to be manually built that has to be done down here
 echo "Getting app..."
 echo "---------------------------------------------------------------"
-itchio-downloader --url "https://rigs-of-rods.itch.itch.io/rigs-of-rods" --platform linux
+itchio-downloader --url "https://rigs-of-rods.itch.io/rigs-of-rods" --platform linux --downloadDirectory .
 mkdir -p ./AppDir/bin
-tar -xvf rigs-of-rods-linux.zip
+tar -xvf game-2066563.zip
 mv -v RoR plugins.cfg resources languages content ./AppDir/bin
 mv -v lib/* /usr/lib
