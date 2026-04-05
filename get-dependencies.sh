ecbb697c35ebb7c82e46b43588661e7e6c94b1c7
Link4Electronics <link4electronics@gmail.com>
2026-04-05 18:25:20 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -31,7 +31,7 @@ patchelf --set-rpath '$ORIGIN/lib' ./RoR
 patchelf --set-rpath '$ORIGIN' ./lib/*.so*
 rm -f ./*.zip ./RunRoR
 
-# TODO find a way to set version automatically, will leavea this as reference in case dev doesn't sync with GH releases
+# TODO find a way to set version automatically, will leave this as reference in case dev doesn't sync with GH releases tag version
 #echo '2026.01' > ~/version
 VERSION="$(git ls-remote --tags --sort="v:refname" https://github.com/RigsOfRods/rigs-of-rods | grep -o '20[0-9]\{2\}\.[0-9]\{2\}' | tail -n1)"
 echo "$VERSION" > ~/version
